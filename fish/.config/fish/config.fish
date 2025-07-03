be2d18e2a298156f7d7010ea014c95f72f96b243
Jeremy Pries <jpries@liveperson.com>
2025-07-03 17:15:37 -0500

    Most of the changes for switching to ghostty, zellij, flashspace, sketchybar

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,19 @@
+# Unset the default fish greeting text which messes up Zellij
+set fish_greeting
+
 if status is-interactive
+    # At this point, specify the Zellij config dir, so we can launch it manually if we want to
+    export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+
+    # Check if our Terminal emulator is Ghostty
+    if [ "$TERM" = xterm-ghostty ]
+        # Launch zellij automatically when ghostty is started
+        eval (zellij setup --generate-auto-start fish | string collect)
+    end
+
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    atuin init fish | source
 end
 
 # maps $code path
